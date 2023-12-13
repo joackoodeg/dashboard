b19bd832c618e753e5983345ff8de859b04dff26
Joaquin Degiovanni <122236268+joackoodeg@users.noreply.github.com>
2023-12-13 20:55:58 -0300

    finish

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})` 
+        document.body.style.backgroundImage = `url(${data.urls.full})` 
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
@@ -19,7 +19,6 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
         return res.json()
     })
     .then(data => {
-        console.log(data)
         document.getElementById("crypto-top").innerHTML = `
             <img src="${data.image.small}" alt="" id="crypto-img">
             <span id="crypto-name">${data.name}</span>
@@ -35,16 +34,31 @@ fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
         console.log(err)
     })
 
-    function getTime(){
-        let currentDate = new Date();
-        let am_pm = currentDate.toLocaleTimeString("en-us", {timeStyle: "short"});
-        document.getElementById("time").textContent = `
-            ${am_pm}
-        `
-    }
+function getTime(){
+    let currentDate = new Date();
+    let am_pm = currentDate.toLocaleTimeString("en-us", {timeStyle: "short"});
+    document.getElementById("time").textContent = `
+        ${am_pm}
+    `
+}
 
-    setInterval(getTime,1000);
+setInterval(getTime,1000);
 
 navigator.geolocation.getCurrentPosition(position => {
-    console.log(position)
-})
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            document.getElementById("weather").innerHTML = `
+                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.log(err))
+});
+
